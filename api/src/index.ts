ac8863804b6d4fd6c9d2c42747539d1e1c743bdb
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2023-01-18 18:05:28 -0300

    websocket added

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,19 @@
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 import { router } from './router';
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
+
 
 mongoose.connect('mongodb://localhost:27017')
     .then(() => {
-        const app = express();
         const port = 3001;
-
 
         app.use((req, res, next) => {
             res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,7 +26,7 @@ mongoose.connect('mongodb://localhost:27017')
         app.use(express.json());
         app.use(router);
 
-        app.listen(port, () => {
+        server.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
     })
