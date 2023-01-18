e59da2386f6bd3a24940d38f29f22db13f1a396e
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2023-01-18 18:06:06 -0300

    websocket added

--- fe/src/components/Orders/index.tsx
+++ fe/src/components/Orders/index.tsx
@@ -1,4 +1,6 @@
 import {useEffect, useState} from 'react';
+import socketIo from 'socket.io-client';
+
 import { Container } from './styles';
 import { OrdersBoard } from '../OrdersBoard';
 import { Order } from '../../types/Order';
@@ -7,6 +9,16 @@ import { api } from '../../utils/api';
 export function Orders () {
 
   const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect( ( ) => {
+    const socket = socketIo('http://localhost:3001', {
+      transports: ['websocket'],
+    });
+
+    socket.on('orders@new', (order) => {
+      setOrders(prevState => prevState.concat(order));
+    });
+  }, []);
 
   useEffect(() => {
     api.get('/orders')
